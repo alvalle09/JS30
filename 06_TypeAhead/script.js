d71bae46618ee73fd596945a1ef38e8b7a4d06b4
alvalle09 <phredvalle@gmail.com>
2018-09-13 23:46:10 -0500

    playing around with fetch & promise

--- 06_TypeAhead/script.js
+++ 06_TypeAhead/script.js
@@ -1,4 +1,9 @@
 
 const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
 
-console.table('Phred', 'codes');
+console.table([["Phred"],["Codes"],["!"]]);
+
+const cities = [];
+
+const prom = fetch(endpoint);
+console.table(prom);
