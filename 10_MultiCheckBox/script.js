693d20a81ec28f846f5b0e7d1c894d0d11538a38
alvalle09 <phredvalle@gmail.com>
2018-09-28 20:04:37 -0500

    adding external js file

---
+++ 10_MultiCheckBox/script.js
@@ -0,0 +1,6 @@
+
+//const checkboxes = document.querySelectorAll('.inbox');
+//const items = document.querySelectorAll('.item');
+
+//select both above in 1 line instead
+const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
