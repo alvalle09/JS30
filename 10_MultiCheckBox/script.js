91c1f6d3de87d0b9e38908d008cdf58525456d65
alvalle09 <phredvalle@gmail.com>
2018-09-28 20:10:44 -0500

    handling click event, console log for now

--- 10_MultiCheckBox/script.js
+++ 10_MultiCheckBox/script.js
@@ -1,6 +1,18 @@
 
-//const checkboxes = document.querySelectorAll('.inbox');
-//const items = document.querySelectorAll('.item');
+// const checkboxes = document.querySelectorAll('.inbox');
+// const items = document.querySelectorAll('.item');
 
-//select both above in 1 line instead
+// select both above in 1 line instead
 const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+
+// handle click event
+function handleCheck(e) {
+    console.log(e);
+}
+
+// loop through each checkbox...click event triggered by keyboard event too
+checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck));
+
+
+
+
