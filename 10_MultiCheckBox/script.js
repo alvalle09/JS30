c1912b4c865d0884f7e4ab989cdb9940534ff3ad
alvalle09 <phredvalle@gmail.com>
2018-09-28 20:18:19 -0500

    check for shift key, and if checkbox is being checked

--- 10_MultiCheckBox/script.js
+++ 10_MultiCheckBox/script.js
@@ -5,13 +5,25 @@
 // select both above in 1 line instead
 const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
 
+// using let because this var will be dynamic
+let lastChecked; 
+
 // handle click event
 function handleCheck(e) {
-    console.log(e);
+    //console.log(e);
+
+    // check if they had shift key down, 
+    // and check that they are checking it
+    if(e.shiftKey && this.checked) {
+
+    }
+
+    lastChecked = this;
 }
 
 // loop through each checkbox...click event triggered by keyboard event too
 checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck));
+
 
 
 
