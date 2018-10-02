3fb4c058d93830f644913057f8157cf748d5d0f3
alvalle09 <phredvalle@gmail.com>
2018-10-01 23:43:35 -0500

    allows multiple checkbox checked using shift key, still see many issues with this logic, will revisit soon

--- 10_MultiCheckBox/script.js
+++ 10_MultiCheckBox/script.js
@@ -11,11 +11,23 @@ let lastChecked;
 // handle click event
 function handleCheck(e) {
     //console.log(e);
+    // lets us track if an item is between 2 other checkboxes
+    let inBetween = false;
 
     // check if they had shift key down, 
     // and check that they are checking it
     if(e.shiftKey && this.checked) {
+        checkboxes.forEach(checkbox => {
+            //console.log(checkbox);
+            if(checkbox === this || checkbox === lastChecked){
+                inBetween = !inBetween;    
+                console.log('Starting in Between!');
+            }
 
+            if (inBetween) {
+                checkbox.checked = true;
+            }
+        });
     }
 
     lastChecked = this;
