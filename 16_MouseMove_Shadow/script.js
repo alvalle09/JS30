c25ded723ba9b8bf7be19b5b0724fd12449b6bc4
alvalle09 <phredvalle@gmail.com>
2019-01-01 23:27:51 -0600

    refactor, move js and css to their own files

---
+++ 16_MouseMove_Shadow/script.js
@@ -0,0 +1,33 @@
+
+const hero = document.querySelector('.hero');
+const text = hero.querySelector('h1');
+// we'll call the amount the shadow should stretch a "walk"
+const walk = 100; // 100px
+
+function shadow(e) {
+    //console.log(e);
+    //const width = hero.offsetWidth;
+    //const height = hero.offsetHeight;
+    //or all in one
+    const {offsetWidth: width, offsetHeight: height} = hero;
+    let { offsetX: x, offsetY: y } = e;
+    // this will give the coordinates of the target
+    //console.log(x, y);
+    // "this" is what we're listening on, but "target" is what triggered event
+    //console.log(this, e.target);
+
+    if (this !== e.target) {
+        x = x + e.target.offsetLeft;
+        y = y + e.target.offsetTop;
+    }
+
+    //console.log(x, y);
+
+    const xWalk = Math.round((x / width * walk) - (walk / 2));
+    const yWalk = Math.round((y / height * walk) - (walk / 2))
+
+    console.log(xWalk, yWalk);
+
+}
+
+hero.addEventListener('mousemove', shadow);
