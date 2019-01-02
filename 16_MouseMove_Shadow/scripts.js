37bd9bb0a35befc987c7e0976b6def77c64f30f0
alvalle09 <phredvalle@gmail.com>
2019-01-01 23:48:01 -0600

    adding textshadow style for effects, using red for now as test

--- 16_MouseMove_Shadow/scripts.js
+++ 16_MouseMove_Shadow/scripts.js
@@ -26,7 +26,9 @@ function shadow(e) {
     const xWalk = Math.round((x / width * walk) - (walk / 2));
     const yWalk = Math.round((y / height * walk) - (walk / 2))
 
-    console.log(xWalk, yWalk);
+    //console.log(xWalk, yWalk);
+    text.style.textShadow = `${xWalk}px ${yWalk}px 0 red`;
+
 
 }
 
