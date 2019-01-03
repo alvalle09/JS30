49175758b8099189afafe64f8783cd9de87031d0
alvalle09 <phredvalle@gmail.com>
2019-01-03 00:44:35 -0600

    add mock data, dynamic background

---
+++ 17_SortList/script.js
@@ -0,0 +1,12 @@
+
+const bands = [ 'The Beatles', 
+                'The Kinks',
+                'Jimmy Hendrix and the Experience',
+                'The Plot in You',
+                'The Devil Wears Prada',
+                'Pierce the Veil', 
+                "The Rolling Stones",
+                "Pink Floyd",
+                "Tom Petty",
+                "An Old Dog"];
+
