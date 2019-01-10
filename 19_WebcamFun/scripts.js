360d3eba0d49e65ccd5a18db86c800a51acc7731
alvalle09 <phredvalle@gmail.com>
2019-01-10 00:29:51 -0600

    adding red effect first

--- 19_WebcamFun/scripts.js
+++ 19_WebcamFun/scripts.js
@@ -32,6 +32,19 @@ function paintToCanvas() {
 
     return setInterval(() => {
         ctx.drawImage(video, 0, 0, width, height);
+        // extract pixels
+        let pixels = ctx.getImageData(0, 0, width, height);
+
+        // console.log(pixels);
+        // pauses execution for debuggig
+        // debugger;
+        // mess with the pixels
+        pixels = redEffect(pixels);
+        //pixels = rgbSplit(pixels);
+
+        // put them back
+        ctx.putImageData(pixels, 0, 0);
+
     }, 16);
 }   
 
@@ -51,6 +64,26 @@ function takePhoto() {
     link.innerHTML = `<img src="${data}" alt="Me" />`;
     strip.insertBefore(link, strip.firstChild);
 
+}
+
+function redEffect(pixels) {
+    for(let i = 0; i < pixels.data.length; i+=4) {
+        pixels.data[i + 0] = pixels.data[i + 0] + 100;     // red  ,adding + 0 for formatting purposes only
+        pixels.data[i + 1] = pixels.data[i + 1] - 50;      // green  
+        pixels.data[i + 2] = pixels.data[i + 2] * 0.5;     // blue
+        // no need for alpha value
+    }
+    return pixels;
+}
+
+function rgbSplit(pixels) {
+    for(let i = 0; i < pixels.data.length; i+=4) {
+        pixels.data[i - 150] = pixels.data[i + 0];     // red  
+        pixels.data[i + 100] = pixels.data[i + 1];      // green  
+        pixels.data[i - 550] = pixels.data[i + 2];     // blue
+        // no need for alpha value
+    }
+    return pixels;
 }
 
 
