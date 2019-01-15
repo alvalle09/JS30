c1a1500b83021820580e09294481c0de0b19be1b
alvalle09 <phredvalle@gmail.com>
2019-01-14 23:16:05 -0600

    fixes mic bug, listening for certain words, replacing Siri???

--- 20_Speech/script.js
+++ 20_Speech/script.js
@@ -3,13 +3,34 @@ window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecogn
 
 const recog = new SpeechRecognition();
 recog.interimResults = true;
+recog.lang = 'en-US';
 
 let p = document.createElement('p');
 const words = document.querySelector('.words');
 words.appendChild(p);
 
 recog.addEventListener('result', e => {
-    console.log(e);
+    //console.log(e.results);
+
+    const transcript = Array.from(e.results) 
+        .map(result => result[0])
+        .map(result => result.transcript)
+        .join('');
+
+    p.textContent = transcript;
+
+    if(e.results[0].isFinal) {
+        p = document.createElement('p');
+        words.appendChild(p);
+    }
+    console.log(transcript);
+
+    if(transcript.includes('dog')) {
+        console.log('🐕🐕🐕🐕🐕🐕🐕🐕🐕🐕🐕🐕🐕🐕🐕🐕🐕🐕 Woof!!!')
+    }
+
 });
+
+recog.addEventListener('end', recog.start);
 
 recog.start();
