9381b68efbafcf7ccea2447a910fe924a5554663
alvalle09 <phredvalle@gmail.com>
2019-01-21 23:48:09 -0600

    set voices dynamically

--- 23_SpeechSynth/script.js
+++ 23_SpeechSynth/script.js
@@ -19,12 +19,21 @@ const stopButton = document.querySelector('#stop');
 msg.text = document.querySelector('[name="text"]').value;
 
 speechSynthesis.addEventListener('voiceschanged', populateVoices);
+voicesDropdown.addEventListener('change', setVoice);
+
 
 function populateVoices() {
    voices = this.getVoices();
    //console.log(voices);
    const voiceOptions = voices
-    .map(voice => `<option value="${voice.name}">${voice.name} (${voice.lang}))</option>`)
+    .map(voice => `<option value="${voice.name}">${voice.name} (${voice.lang})</option>`)
     .join('');
    voicesDropdown.innerHTML = voiceOptions;
+}
+
+function setVoice() {
+    //console.log('Changing voice');
+    //console.log(this.value); //gets name property only
+    msg.voice = voices.find(voice => voice.name === this.value);
+
 }
