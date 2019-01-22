0ac05966e6a43c0c355665bb39fed64cfd9167c5
alvalle09 <phredvalle@gmail.com>
2019-01-21 23:32:41 -0600

    adds speechSynthesis speak via console so far only

--- 23_SpeechSynth/script.js
+++ 23_SpeechSynth/script.js
@@ -1,7 +1,30 @@
-
+/*
 const msg = new SpeechSynthesisUtterance();
+
 let voices = [];
 const voicesDropdown = document.querySelector('[name="voice"]');
 const options = document.querySelectorAll('[type="range"], [name="text"]');
 const stopButton = document.querySelector('#stop');
 const speakButton = document.querySelector('#speak');
+
+
+msg.text = document.querySelector('[name="text"]').value;*/
+
+const msg = new SpeechSynthesisUtterance();
+let voices = [];
+const voicesDropdown = document.querySelector('[name="voice"]');
+const options = document.querySelectorAll('[type="range"], [name="text"]');
+const speakButton = document.querySelector('#speak');
+const stopButton = document.querySelector('#stop');
+msg.text = document.querySelector('[name="text"]').value;
+
+speechSynthesis.addEventListener('voiceschanged', populateVoices);
+
+function populateVoices() {
+   voices = this.getVoices();
+   //console.log(voices);
+   const voiceOptions = voices
+    .map(voice => `<option value="${voice.name}">${voice.name} (${voice.lang}))</option>`)
+    .join('');
+   voicesDropdown.innerHTML = voiceOptions;
+}
