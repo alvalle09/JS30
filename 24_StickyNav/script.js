f53018dacd97ef8ba7cb053ff62208a093bbc17d
alvalle09 <phredvalle@gmail.com>
2019-01-23 00:48:22 -0600

    fixes padding top for smooth sticky nav, also log transition in

--- 24_StickyNav/script.js
+++ 24_StickyNav/script.js
@@ -7,8 +7,12 @@ function fixNav() {
     console.log(topOfNav,  window.scrollY);
     if (window.scrollY >= topOfNav) {
        document.body.classList.add('fixed-nav');
+       // cant' use "padding-top" in js, use camelCase instead
+       // also, don't hardcode the paddingtop prop to something like 74px, size of nav could change based on font, etc..
+       document.body.style.paddingTop = nav.offsetHeight + 'px';
     } else {
         document.body.classList.remove('fixed-nav');
+        document.body.style.paddingTop = 0;
     }
 }
 
