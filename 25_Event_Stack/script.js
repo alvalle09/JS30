9f7b2aa6d5d3c7d152281e4b1f3bafc383c3ffde
alvalle09 <phredvalle@gmail.com>
2019-01-26 15:29:22 -0600

    js30 #25_Eventstack init commit

---
+++ 25_Event_Stack/script.js
@@ -0,0 +1,11 @@
+
+const divs = document.querySelectorAll('div');
+
+function logText(e) {
+    //console.log(this.classList.value);
+    console.log(this);
+}
+
+divs.forEach(div => div.addEventListener('click', logText));
+
+
