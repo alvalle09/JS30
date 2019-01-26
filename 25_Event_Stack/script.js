51b2af37141922309d5b67f6c49572647ab8c3ba
alvalle09 <phredvalle@gmail.com>
2019-01-26 15:56:44 -0600

    event capture, propagation, bubbling, once, final commit

--- 25_Event_Stack/script.js
+++ 25_Event_Stack/script.js
@@ -1,14 +1,26 @@
 
 const divs = document.querySelectorAll('div');
+const button = document.querySelector('button');
+
 
 function logText(e) {
     console.log(this.classList.value);
    //console.log(this);
-   e.stopPropagation();
+   //e.stopPropagation(); // stop bubbling
 }
 
-document.body.addEventListener('click', logText);
+//document.body.addEventListener('click', logText);
 
-divs.forEach(div => div.addEventListener('click', logText));
+divs.forEach(div => div.addEventListener('click', logText, {
+    capture: false,
+    once: true  // run function on way down, not up
+}));
+
+button.addEventListener('click', () => {
+    console.log('Clicked!!!');
+}, {
+    once: true
+});
+
 
 
