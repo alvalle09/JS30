701e6a932d0a7def38893a815854790c3a677b5a
alvalle09 <phredvalle@gmail.com>
2019-01-26 15:39:21 -0600

    prevent propogation of events

--- 25_Event_Stack/script.js
+++ 25_Event_Stack/script.js
@@ -2,9 +2,12 @@
 const divs = document.querySelectorAll('div');
 
 function logText(e) {
-    //console.log(this.classList.value);
-    console.log(this);
+    console.log(this.classList.value);
+   //console.log(this);
+   e.stopPropagation();
 }
+
+document.body.addEventListener('click', logText);
 
 divs.forEach(div => div.addEventListener('click', logText));
 
