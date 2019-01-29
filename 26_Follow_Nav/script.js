88ee7094e8ba5360de35bbb79ead1b15a353a10c
alvalle09 <phredvalle@gmail.com>
2019-01-28 21:42:04 -0600

    Cool transitions using css and js

--- 26_Follow_Nav/script.js
+++ 26_Follow_Nav/script.js
@@ -1,19 +1,43 @@
+
+
 const triggers = document.querySelectorAll('.cool > li');
-const backgroung = document.querySelector('.dropdownBackground');
+const background = document.querySelector('.dropdownBackground');
 const nav = document.querySelector('.top');
 
 function handleEnter() {
-    console.log('enter!@');
+    // console.log('enter!@');
     this.classList.add('trigger-enter');
-    setTimeout(() => {
-        console.log(this);
-        this.classList.add('trigger-enter-active');  
-    }, 150);
+    // delay active class 150 milliseconds
+    setTimeout(() => this.classList.add('trigger-enter-active'), 150);
+    background.classList.add('open');
+
+    const dropdown = this.querySelector('.dropdown');
+    //console.log(dropdown);
+    const dropdownCoords = dropdown.getBoundingClientRect();
+    // get coords of nav too, to add to position relative to nav
+    const navcoords = nav.getBoundingClientRect();
+    //console.log(dropdownCoords);
+    console.log(navcoords);
+
+    const coords = {
+        height: dropdownCoords.height,
+        width: dropdownCoords.width
+    };
+    // needs pixels, so use template strings
+    background.style.setProperty('height', `${coords.height}px`);
+    background.style.setProperty('width', `${coords.width}px`);
+
+
 }
 
 
 function handleLeave() {
-    console.log('Leaving!@');
+    // console.log('Leaving!');
+    this.classList.remove('trigger-enter', 'trigger-enter-active');
+    background.classList.remove('open');
+
+    
+
 }
 
 triggers.forEach(trigger => trigger.addEventListener('mouseenter', handleEnter));
