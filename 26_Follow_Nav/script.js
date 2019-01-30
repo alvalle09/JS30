aa680a8b156549bf56e67b31e0a491c913a14842
alvalle09 <phredvalle@gmail.com>
2019-01-29 21:23:23 -0600

    js30 #27 click, drag and scroll, init commit

--- 26_Follow_Nav/script.js
+++ 26_Follow_Nav/script.js
@@ -47,9 +47,8 @@ function handleLeave() {
     this.classList.remove('trigger-enter', 'trigger-enter-active');
     background.classList.remove('open');
 
-    
-
 }
+
 
 triggers.forEach(trigger => trigger.addEventListener('mouseenter', handleEnter));
 triggers.forEach(trigger => trigger.addEventListener('mouseleave', handleLeave));
