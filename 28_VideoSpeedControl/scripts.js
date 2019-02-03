b19df3783a23f2ea542c6434d046ad90db3f749a
alvalle09 <phredvalle@gmail.com>
2019-02-03 15:18:08 -0600

    set bar height, and playbackrate

--- 28_VideoSpeedControl/scripts.js
+++ 28_VideoSpeedControl/scripts.js
@@ -5,4 +5,20 @@ const bar = speed.querySelector('.speed-bar');
 const video = document.querySelector('.flex');
 
 
- 
+ speed.addEventListener('mousemove', function(e) {
+    //console.log(e);
+    const y = e.pageY - this.offsetTop;
+    //console.log(y);
+    const percent = y / this.offsetHeight;    
+    //console.log(percent);
+    const min = 0.4;
+    const max = 4;
+
+    const height = Math.round(percent * 100) + '%';
+    console.log(height);
+    bar.style.height = height;
+    const playbackRate = percent * (max - min) + min;
+    bar.textContent = playbackRate.toFixed(2) + 'X';
+    console.log(playbackRate);
+    video.playbackRate = playbackRate;
+ })
