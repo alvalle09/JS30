bb2577c78d6eb119001489f4381ee53959967280
alvalle09 <phredvalle@gmail.com>
2019-02-03 15:35:59 -0600

    refactor mousemove function, handle event in separate function

--- 28_VideoSpeedControl/scripts.js
+++ 28_VideoSpeedControl/scripts.js
@@ -4,21 +4,23 @@ const speed = document.querySelector('.speed');
 const bar = speed.querySelector('.speed-bar');
 const video = document.querySelector('.flex');
 
+function handleMove(e) {
+     //console.log(e);
+     const y = e.pageY - this.offsetTop;
+     //console.log(y);
+     const percent = y / this.offsetHeight;    
+     //console.log(percent);
+     const min = 0.4;
+     const max = 4;
+ 
+     const height = Math.round(percent * 100) + '%';
+     console.log(height);
+     bar.style.height = height;
+     const playbackRate = percent * (max - min) + min;
+     bar.textContent = playbackRate.toFixed(2) + 'X';
+     console.log(playbackRate);
+     video.playbackRate = playbackRate;
+}
 
- speed.addEventListener('mousemove', function(e) {
-    //console.log(e);
-    const y = e.pageY - this.offsetTop;
-    //console.log(y);
-    const percent = y / this.offsetHeight;    
-    //console.log(percent);
-    const min = 0.4;
-    const max = 4;
-
-    const height = Math.round(percent * 100) + '%';
-    console.log(height);
-    bar.style.height = height;
-    const playbackRate = percent * (max - min) + min;
-    bar.textContent = playbackRate.toFixed(2) + 'X';
-    console.log(playbackRate);
-    video.playbackRate = playbackRate;
- })
+ speed.addEventListener('mousemove', handleMove);
+ //here we can add other events such as touchMove, etc..
