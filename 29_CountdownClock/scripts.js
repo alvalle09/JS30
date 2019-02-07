5e0b5f8296f74557c5738d834ba0eb68c5a53245
alvalle09 <phredvalle@gmail.com>
2019-02-06 23:31:53 -0600

    use var to control setinterval

--- 29_CountdownClock/scripts.js
+++ 29_CountdownClock/scripts.js
@@ -1,10 +1,24 @@
 
+let countdown;
+
+
 function timer(seconds) {
     const now = Date.now();
     const then = now + seconds * 1000;
 
-    setInterval(() => {
-        const secondsLeft = (then - Date.now()) /1000; // run date.now again to get current time, divide by 1k to get seconds
+    countdown = setInterval(() => {
+        const secondsLeft = Math.round((then - Date.now()) /1000); // run date.now again to get current time, divide by 1k to get seconds
+        // check if we need to stop running
+        /*
+        if (secondsLeft <= 0) {
+            return;
+        } */   /// this doesn't stop even from running, it just returns nothing      
+
+        // can't be <= 0, otherwise will still give -1 seconds
+        if (secondsLeft < 0) {        
+            clearInterval(countdown);
+            return;
+        }
         console.log(secondsLeft);
     }, 1000);
 }
