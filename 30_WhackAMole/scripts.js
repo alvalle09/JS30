3fed4e0dc58188e4ff111a23378f7203661ded49
alvalle09 <phredvalle@gmail.com>
2019-02-09 17:54:08 -0600

    Adding random time for showing moles, and randomHole for showing at random holes

--- 30_WhackAMole/scripts.js
+++ 30_WhackAMole/scripts.js
@@ -2,4 +2,23 @@
 const holes = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelector('.score');
 const moles = document.querySelectorAll('.mole');
+let lastHole;
 
+function randomTime(min, max) {
+    return Math.round(Math.random() * (max - min ) + min);
+}
+
+function randomHole(holes) {
+    const idx = Math.floor(Math.random() * holes.length);
+    //console.log(holes.length);
+    const hole = holes[idx];
+
+    if (hole === lastHole) {
+        console.log('Same hole!');
+        return randomHole(holes);
+    }
+    
+    //console.log(hole);
+    lastHole = hole;
+    return hole;
+}
