71a7ac9a68f6ed4f5de839ab966766c74959a939
alvalle09 <phredvalle@gmail.com>
2019-02-08 23:48:18 -0600

    set end time display

--- 29_CountdownClock/scripts.js
+++ 29_CountdownClock/scripts.js
@@ -1,12 +1,15 @@
 
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
 
 function timer(seconds) {
     const now = Date.now();
     const then = now + seconds * 1000;
     console.log({now, then});
     displayTimeLeft(seconds);
+    // only need to run once
+    displayEndTime(then);
 
     countdown = setInterval(() => {
         const secondsLeft = Math.round((then - Date.now()) /1000); // run date.now again to get current time, divide by 1k to get seconds
@@ -36,3 +39,10 @@ function displayTimeLeft(seconds) {
     console.log({minutes, remainderSeconds});
 }
 
+function displayEndTime(timestamp) {
+    const end = new Date(timestamp);
+    const hour = end.getHours();
+    const minutes = end.getMinutes();
+    endTime.textContent = `Be Back At ${hour}:${minutes}`;
+
+}
