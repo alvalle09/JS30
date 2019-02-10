fcb9fc01d565f6a43effc25a8f26283b6568ccb7
alvalle09 <phredvalle@gmail.com>
2019-02-09 23:23:51 -0600

    disable start play button until current game is over

--- 30_WhackAMole/scripts.js
+++ 30_WhackAMole/scripts.js
@@ -3,6 +3,7 @@ const holes = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelector('.score');
 const moles = document.querySelectorAll('.mole');
 const timeKeeper = document.querySelector('.timeleft');
+const btnStart = document.querySelector('.start');
 
 let lastHole;
 let timeUp = false;
@@ -37,7 +38,7 @@ function startTimer() {
 
 function displayTimeLeft(seconds) {
     
-    timeKeeper.textContent = seconds;
+    timeKeeper.textContent = `${seconds} seconds left`;
     //document.title = `${'🕓'} ${display}`;
 
 }
@@ -48,15 +49,13 @@ function timer(seconds) {
     let secondsLeft = seconds;
 
     countdown = setInterval(() => {
-        secondsLeft--; // run date.now again to get current time, divide by 1k to get seconds
+        secondsLeft--; 
         // check if we need to stop running
         /*
-        if (secondsLeft <= 0) {
-            return;
-        } */   /// this doesn't stop even from running, it just returns nothing      
+        if (secondsLeft <= 0) return;   */   /// this doesn't stop even from running, it just returns nothing      
 
-        // can't be < 0, otherwise will still give -1 seconds
-        if (secondsLeft <= 0) {        
+        // can't be <= 0, otherwise will still give -1 seconds
+        if (secondsLeft < 0) {        
             clearInterval(countdown);
             return;
         }
@@ -76,16 +75,25 @@ function peep() {
     setTimeout(() => {
         hole.classList.remove('up');
         // if game isn't over, keep repeating
-       if (!timeUp) peep();
+       if (!timeUp) { peep()}
+       else {
+        btnStart.disabled = false;
+       };
     }, time);
 }
 
 function startGame() {
+    btnStart.disabled = true;
     scoreBoard.textContent = 0;
     score = 0;
     timeUp = false;
-    peep();    
+
+    peep();
+    // end after 10 seconds
     setTimeout(() => timeUp = true, 10000);
+    // show time left
+    timeKeeper.style.display = 'block';
+
     startTimer();
 }
 
@@ -93,11 +101,11 @@ function startGame() {
 
 function bonk(e) {
     if (!e.isTrusted) return; // this only true when actual click from user, not simulated in js
-    console.log(e);
+    //console.log(e);
     score++;
     this.classList.remove('up');
     scoreBoard.textContent = score;
 }
 
-moles.forEach(mole => mole.addEventListener('click', bonk));
-//moles.forEach(mole => mole.addEventListener('touchstart', bonk));
+//moles.forEach(mole => mole.addEventListener('click', bonk));
+moles.forEach(mole => mole.addEventListener('touchstart', bonk));
