f3d4ec05661872ec35ec8a55cad93cc512e6acb9
alvalle09 <phredvalle@gmail.com>
2019-02-09 18:54:53 -0600

    first attempt at showing timer, borrowed from js#29, still debugging

--- 30_WhackAMole/scripts.js
+++ 30_WhackAMole/scripts.js
@@ -2,9 +2,13 @@
 const holes = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelector('.score');
 const moles = document.querySelectorAll('.mole');
+const timeKeeper = document.querySelector('.timeleft');
+
 let lastHole;
 let timeUp = false;
 let score = 0;
+let totalTime = 10;
+let countdown;
 
 function randomTime(min, max) {
     return Math.round(Math.random() * (max - min ) + min);
@@ -25,10 +29,49 @@ function randomHole(holes) {
     return hole;
 }
 
+function startTimer() {
+    const seconds = 10;
+    timer(seconds);
+    //console.log(seconds);
+}
+
+function displayTimeLeft(seconds) {
+    
+    timeKeeper.textContent = seconds;
+    //document.title = `${'🕓'} ${display}`;
+
+}
+
+function timer(seconds) {
+    // clear any timers first
+    clearInterval(countdown);
+    let secondsLeft = seconds;
+
+    countdown = setInterval(() => {
+        secondsLeft--; // run date.now again to get current time, divide by 1k to get seconds
+        // check if we need to stop running
+        /*
+        if (secondsLeft <= 0) {
+            return;
+        } */   /// this doesn't stop even from running, it just returns nothing      
+
+        // can't be < 0, otherwise will still give -1 seconds
+        if (secondsLeft <= 0) {        
+            clearInterval(countdown);
+            return;
+        }
+        //console.log(secondsLeft);
+        displayTimeLeft(secondsLeft);
+    }, 1000);
+} 
+
+
+
 function peep() {
     const time = randomTime(200, 1000);
     const hole = randomHole(holes);
     console.log(time, hole);
+    
     hole.classList.add('up');
     setTimeout(() => {
         hole.classList.remove('up');
@@ -41,9 +84,12 @@ function startGame() {
     scoreBoard.textContent = 0;
     score = 0;
     timeUp = false;
-    peep();
+    peep();    
     setTimeout(() => timeUp = true, 10000);
+    startTimer();
 }
+
+//setTimeout(() => timeKeeper.textContent = totalTime--, 1000);
 
 function bonk(e) {
     if (!e.isTrusted) return; // this only true when actual click from user, not simulated in js
@@ -54,3 +100,4 @@ function bonk(e) {
 }
 
 moles.forEach(mole => mole.addEventListener('click', bonk));
+//moles.forEach(mole => mole.addEventListener('touchstart', bonk));
